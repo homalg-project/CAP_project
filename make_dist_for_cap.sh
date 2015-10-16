29c4f0f5bec2a74470ddd1d45905a0e329375bdb
Sebastian Gutsche <gutsche@momo.math.rwth-aachen.de>
2015-10-16 10:56:34 +0200

    Changed name of index_default

--- make_dist_for_cap.sh
+++ make_dist_for_cap.sh
@@ -66,7 +66,7 @@ cd $current_dir
 
 for i in $packages; do
   cp ${i}/_data/package.yml _data/package${i}.yml
-  sed "s|@@package@@|package${i}|g" < index_default.md > ${i}/index.md
+  sed "s|@@package@@|package${i}|g" < index_default.template > ${i}/index.md
 done
 
 git add *
