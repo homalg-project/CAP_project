d9979e4304a4da92254cb4536964bff9f3ab27d2
Sebastian Gutsche <gutsche@momo.math.rwth-aachen.de>
2015-10-30 10:22:42 +0100

    Updated homepage script to be the same as homalg_project

--- make_dist_for_cap.sh
+++ make_dist_for_cap.sh
@@ -6,6 +6,12 @@ current_dir=$(pwd)
 
 for i in $packages; do
   cd $i
+  cd ..
+  mkdir -p tmp
+  git archive --format=tar --output=tmp/${i}.tar --prefix=${i}/ HEAD:${i}
+  cd tmp
+  tar xf ${i}.tar
+  cd ${i}
 gap -A -q -b <<EOF
 # HACK
 MakeReadWriteGlobal("SetPackageInfo");
@@ -18,11 +24,18 @@ EOF
 
   version=$(cat VERSION)
   rm VERSION
-  make doc
+  gap makedoc.g
+  rm -rf .git*
+  rm -f doc/*.{aux,bbl,blg,brf,idx,ilg,ind,lab,log,out,pnr,tex,toc,tst}
+  rm -rf bin/
+  rm -rf public_html
   cd ..
   tar czvf ${i}-${version}.tar.gz ${i}
-  rm gh-pages/${i}/*tar.gz
-  mv ${i}-${version}.tar.gz gh-pages/${i}
+  mkdir -p ../gh-pages/${i}
+  rm ../gh-pages/${i}/*tar.gz
+  mv ${i}-${version}.tar.gz ../gh-pages/${i}
+  cd ..
+  rm -rf tmp
   cd $current_dir
 done
 
@@ -35,8 +48,8 @@ for i in $packages; do
 done
 
 for i in $packages; do
-  mkdir ${i}
-  mkdir ${i}/doc
+  mkdir -p ${i}
+  mkdir -p ${i}/doc
   cp -f ../${i}/PackageInfo.g ${i}
   cp -f ../${i}/README ${i}
   cp -r ../${i}/doc/*.{css,html,js,txt} ${i}/doc
@@ -46,7 +59,7 @@ current_dir=$(pwd)
 
 for i in $packages; do
   cd ${i}
-  mkdir _data
+  mkdir -p _data
   cp ../_data/package.yml _data
   gap ../update.g
   cd $current_dir
@@ -70,6 +83,14 @@ for i in $packages; do
   sed "s|@@package@@|package${i}|g" < index_default.template > ${i}/index.md
 done
 
-git add *
-git commit -a -m "New version of homepage"
-git push homalg gh-pages:gh-pages
+log_output=$(git log -n 1 --oneline | grep "New version of homepage from dist script")
+
+if [ -n "$log_output" ]; then
+  git add *
+  git commit -a --amend -m "New version of homepage from dist script"
+  git push --force homalg gh-pages:gh-pages
+else
+  git add *
+  git commit -a -m "New version of homepage from dist script"
+  git push homalg gh-pages:gh-pages
+fi
