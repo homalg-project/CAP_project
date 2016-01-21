6f2b88aed78a0d0db911cce16019385525134508
Sebastian Gutsche <gutsche@momo.math.rwth-aachen.de>
2016-01-21 18:10:39 +0100

    Added a higher version for CAP

--- make_dist_for_cap.sh
+++ make_dist_for_cap.sh
@@ -5,7 +5,7 @@ packages="CAP GeneralizedMorphismsForCAP LinearAlgebraForCAP ModulePresentations
 base_dir="$PWD"
 
 for i in ${packages}; do
-  ./release --srcdir ${base_dir}/${i} --webdir ${base_dir}/gh-pages/${i} --update-file ${base_dir}/gh-pages/update
+  ./release --srcdir ${base_dir}/${i} --webdir ${base_dir}/gh-pages/${i} --update-file ${base_dir}/gh-pages/update.g
 done
 
 exit 0
