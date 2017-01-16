1cca4c77ce684dee93f8bf2f902b74fededf9ae9
Sebastian Posur <sebastian.posur@rwth-aachen.de>
2017-01-16 14:01:44 +0100

    Write Documentation

--- GroupRepresentationsForCAP/gap/SemisimpleCategoryObjects.gd
+++ GroupRepresentationsForCAP/gap/SemisimpleCategoryObjects.gd
@@ -36,9 +36,21 @@ DeclareCategory( "IsSemisimpleCategoryObject",
 #! The output is the object in $C$
 #! given by $\oplus_{j=1}^l a_j \chi^{i_j}$.
 #! @Returns an object
-#! @Arguments L
+#! @Arguments L, C
 DeclareOperation( "SemisimpleCategoryObject", [ IsList, IsCapCategory ] );
 
+#! @Description
+#! The arguments are 
+#! a list $L$
+#! and a semisimple category $C = \bigoplus_{i \in I} k\mathrm{-vec}$.
+#! The list $L$ contains an even number of
+#! elements $L = [ a_1, i_1, \dots, a_l, i_l ]$
+#! of non-negative integers $a_j$ and
+#! objects $i_j \in I$.
+#! The output is the object in $C$
+#! given by $\oplus_{j=1}^l a_j \chi^{i_j}$.
+#! @Returns an object
+#! @Arguments L, C
 DeclareOperationWithCache( "SemisimpleCategoryObjectConstructorWithFlatList", [ IsList, IsCapCategory ] );
 
 
@@ -48,26 +60,50 @@ DeclareOperationWithCache( "SemisimpleCategoryObjectConstructorWithFlatList", [ 
 ##
 ####################################
 
-##
+#! @Description
+#! The argument is an object $v = \oplus_{j=1}^l a_j \chi^{i_j}$
+#! in a semisimple category.
+#! The output is the list $[ [ a_1, i_1 ], \dots [ a_l, i_l ] ]$.
+#! @Returns a list
+#! @Arguments v
 DeclareAttribute( "SemisimpleCategoryObjectList",
                   IsSemisimpleCategoryObject );
 
-##
+#! @Description
+#! The argument is an object $v = \oplus_{j=1}^l a_j \chi^{i_j}$
+#! in a semisimple category.
+#! The output is the list $[ [ a_1, \chi^{i_1} ], \dots [ a_l, \chi^{i_l} ] ]$.
+#! @Returns a list
+#! @Arguments v
 DeclareAttribute( "SemisimpleCategoryObjectListWithActualObjects",
                   IsSemisimpleCategoryObject );
 
 #! @Description
-#! The argument is an object $A$ in a semisimple category.
+#! The argument is an object $v = \oplus_{j=1}^l a_j \chi^{i_j}$
+#! in a semisimple category.
+#! The output is the list $[ i_1, \dots, i_l ]$.
 #! @Returns a list
-#! @Arguments A
+#! @Arguments v
 DeclareAttribute( "Support",
                   IsSemisimpleCategoryObject );
 
-##
+#! @Description
+#! The argument is an object $v = \oplus_{j=1}^l a_j \chi^{i_j}$
+#! in a semisimple category $\bigoplus_{i \in I} k\mathrm{-vec}$.
+#! The output is the homalg field $k$.
+#! @Returns a homalg field
+#! @Arguments v
 DeclareAttribute( "UnderlyingFieldForHomalg",
                   IsSemisimpleCategoryObject );
 
-##
+#! @Description
+#! The argument is an object $v = \oplus_{j=1}^l a_j \chi^{i_j}$
+#! in a semisimple category $\bigoplus_{i \in I} k\mathrm{-vec}$.
+#! The output is the integer $\sum_{j=1}^l a_j \cdot \mathrm{dim}( i_j )$.
+#! This functions works under the assumption that there is
+#! a notion of dimension on the objects in $I$.
+#! @Returns an integer
+#! @Arguments v
 DeclareAttribute( "Dimension",
                   IsSemisimpleCategoryObject );
 
