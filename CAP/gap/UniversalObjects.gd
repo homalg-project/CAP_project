b9e8a0b400a2f11f6eea36e39dd0ce5d0d256741
Mohamed Barakat <mohamed.barakat@rwth-aachen.de>
2018-09-18 15:59:38 +0200

    outsourced Associator*OfDirectProducts* to the new package Toposes
    currently the three packages:
    * FinSetsForCAP
    * FinGSetsForCAP
    * Locales
    will (directly) depend on Toposes

--- CAP/gap/UniversalObjects.gd
+++ CAP/gap/UniversalObjects.gd
@@ -2314,68 +2314,6 @@ DeclareOperation( "AddDirectProductFunctorialWithGivenDirectProducts",
 DeclareOperation( "AddDirectProductFunctorialWithGivenDirectProducts",
                   [ IsCapCategory, IsList ] );
 
-##
-#! @Description
-#! The arguments are an object $s = a \times (b \times c)$,
-#! three objects $a,b,c$,
-#! and an object $r = (a \times b) \times c$.
-#! The output is the associator $\alpha_{a,(b,c)}: a \times (b \times c) \rightarrow (a \times b) \times c$.
-#! @Returns a morphism in $\mathrm{Hom}( a \times (b \times c), (a \times b) \times c )$.
-#! @Arguments s, a, b, c, r
-DeclareOperation( "AssociatorRightToLeftOfDirectProductsWithGivenDirectProducts",
-                  [ IsCapCategoryObject, IsCapCategoryObject, IsCapCategoryObject, IsCapCategoryObject, IsCapCategoryObject ] );
-
-#! @Description
-#! The arguments are a category $C$ and a function $F$.
-#! This operations adds the given function $F$
-#! to the category for the basic operation <C>AssociatorRightToLeftOfDirectProductsWithGivenDirectProducts</C>.
-#! $F: ( a \times (b \times c), a, b, c, (a \times b) \times c ) \mapsto \alpha_{a,(b,c)}$.
-#! @Returns nothing
-#! @Arguments C, F
-DeclareOperation( "AddAssociatorRightToLeftOfDirectProductsWithGivenDirectProducts",
-                  [ IsCapCategory, IsFunction ] );
-
-DeclareOperation( "AddAssociatorRightToLeftOfDirectProductsWithGivenDirectProducts",
-                  [ IsCapCategory, IsFunction, IsInt ] );
-
-DeclareOperation( "AddAssociatorRightToLeftOfDirectProductsWithGivenDirectProducts",
-                  [ IsCapCategory, IsList, IsInt ] );
-
-DeclareOperation( "AddAssociatorRightToLeftOfDirectProductsWithGivenDirectProducts",
-                  [ IsCapCategory, IsList ] );
-
-
-##
-#! @Description
-#! The arguments are an object $s = (a \times b) \times c$,
-#! three objects $a,b,c$,
-#! and an object $r = a \times (b \times c)$.
-#! The output is the associator $\alpha_{(a,b),c}: (a \times b) \times c \rightarrow a \times (b \times c)$.
-#! @Returns a morphism in $\mathrm{Hom}( (a \times b) \times c \rightarrow a \times (b \times c) )$.
-#! @Arguments s, a, b, c, r
-DeclareOperation( "AssociatorLeftToRightOfDirectProductsWithGivenDirectProducts",
-                  [ IsCapCategoryObject, IsCapCategoryObject, IsCapCategoryObject, IsCapCategoryObject, IsCapCategoryObject ] );
-
-#! @Description
-#! The arguments are a category $C$ and a function $F$.
-#! This operations adds the given function $F$
-#! to the category for the basic operation <C>AssociatorLeftToRightOfDirectProductsWithGivenDirectProducts</C>.
-#! $F: (( a \times b ) \times c, a, b, c, a \times (b \times c )) \mapsto \alpha_{(a,b),c}$.
-#! @Returns nothing
-#! @Arguments C, F
-DeclareOperation( "AddAssociatorLeftToRightOfDirectProductsWithGivenDirectProducts",
-                  [ IsCapCategory, IsFunction ] );
-
-DeclareOperation( "AddAssociatorLeftToRightOfDirectProductsWithGivenDirectProducts",
-                  [ IsCapCategory, IsFunction, IsInt ] );
-
-DeclareOperation( "AddAssociatorLeftToRightOfDirectProductsWithGivenDirectProducts",
-                  [ IsCapCategory, IsList, IsInt ] );
-
-DeclareOperation( "AddAssociatorLeftToRightOfDirectProductsWithGivenDirectProducts",
-                  [ IsCapCategory, IsList ] );
-
-
 #! @Chapter Universal Objects
 
 ####################################
