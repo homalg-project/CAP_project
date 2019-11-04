7a50def07aaffd073accd2c14632391ab25c7334
Kamal Saleh <ksaleh87@yahoo.com>
2019-11-04 16:48:04 +0100

    Adjust documentation to CAP style
    https://github.com/homalg-project/CAP_project/pull/415/files#r342044511
    https://github.com/homalg-project/CAP_project/pull/415/files#r342047172

--- FreydCategoriesForCAP/gap/AdditiveClosure.gd
+++ FreydCategoriesForCAP/gap/AdditiveClosure.gd
@@ -17,18 +17,21 @@
 #! @Description
 #! The GAP category of additive closures of Ab-categories.
 #! @Arguments object
+#! @Returns true or false
 DeclareCategory( "IsAdditiveClosureCategory",
                  IsCapCategory );
 
 #! @Description
 #! The GAP category of objects in additive closures of Ab-categories.
 #! @Arguments object
+#! @Returns true or false
 DeclareCategory( "IsAdditiveClosureObject",
                  IsCapCategoryObject );
 
 #! @Description
 #! The GAP category of morphisms in additive closures of Ab-categories.
 #! @Arguments object
+#! @Returns true or false
 DeclareCategory( "IsAdditiveClosureMorphism",
                  IsCapCategoryMorphism );
 
@@ -42,72 +45,72 @@ DeclareGlobalFunction( "INSTALL_FUNCTIONS_FOR_ADDITIVE_CLOSURE" );
 
 
 #! @Description
-#! The input is an Ab-category $C$. The output is its additive closure $C^\oplus$.
+#! The argument is an Ab-category $C$. The output is its additive closure $C^\oplus$.
 #! @Arguments C
-#! @Returns a CapCategory
+#! @Returns the category $C^\oplus$
 DeclareAttribute( "AdditiveClosure",
                   IsCapCategory );
 
 #! @Description
-#! The input is a list of objects $L=[A_1,\dots,A_n]$ in an Ab-category $C$. The output is the formal direct sum
+#! The argument is a list of objects $L=[A_1,\dots,A_n]$ in an Ab-category $C$. The output is the formal direct sum
 #! $A_1\oplus\dots\oplus A_n$ in the additive closure $C^\oplus$.
 #! @Arguments L, C^\oplus
-#! @Returns a CapCategoryObject
+#! @Returns an object in $C^\oplus$
 DeclareOperation( "AdditiveClosureObject",
                   [ IsList, IsAdditiveClosureCategory ] );
 
 #! @Description
-#! The input is an object $A$ in an Ab-category $C$. The output is the image of $A$ under the inclusion functor $\iota:C\to C^\oplus$.
+#! The argument is an object $A$ in an Ab-category $C$. The output is the image of $A$ under the inclusion functor $\iota:C\to C^\oplus$.
 #! @Arguments A
-#! @Returns a CapCategoryObject
+#! @Returns an object in $C^\oplus$
 DeclareAttribute( "AsAdditiveClosureObject",
                   IsCapCategoryObject );
 
 #! @Description
-#! The input is a formal direct sums $A=A_1\oplus\dots\oplus A_m$, $B=B_1\oplus\dots\oplus B_n$ and 
-#! an $m\times n$ matrix $M :=(\alpha_{ij}:A_i\to B_j)_{ij}$ for $i=1,\dots,m,j=1,\dots,n$.
+#! The arguments are formal direct sums $A=A_1\oplus\dots\oplus A_m$, $B=B_1\oplus\dots\oplus B_n$ in some additive category $C^\oplus$
+#! and an $m\times n$ matrix $M :=(\alpha_{ij}:A_i\to B_j)_{ij}$ for $i=1,\dots,m,j=1,\dots,n$.
 #! The output is the formal morphism between $A$ and $B$ that is defined by $M$.
 #! @Arguments A, M, B
-#! @Returns a CapCategoryMorphism
+#! @Returns a morphism in $\mathrm{Hom}_{C^\oplus}(A,B)$
 DeclareOperation( "AdditiveClosureMorphism",
                   [ IsAdditiveClosureObject, IsList, IsAdditiveClosureObject ] );
 
 #! @Description
-#! The input is a morphism $\alpha$ in an Ab-category $C$. The output is the image of $\alpha$ under the inclusion functor $\iota:C\to C^\oplus$.
+#! The argument is a morphism $\alpha$ in an Ab-category $C$. The output is the image of $\alpha$ under the inclusion functor $\iota:C\to C^\oplus$.
 #! @Arguments alpha
-#! @Returns a CapCategoryMorphism
+#! @Returns a morphism in $C^\oplus$
 DeclareAttribute( "AsAdditiveClosureMorphism",
                   IsCapCategoryMorphism );
 
 #! @Description
-#! The input is an Ab-category $C$. The output is the inclusion functor $\iota:C\to C^\oplus$.
+#! The argument is an Ab-category $C$. The output is the inclusion functor $\iota:C\to C^\oplus$.
 #! @Arguments C
-#! @Returns a CapFunctor
+#! @Returns a functor $C\to C^\oplus$
 DeclareAttribute( "InclusionFunctorInAdditiveClosure",
                   IsCapCategory );
 
 #! @Description
-#! The input is a functor $F:C\to D$, and the output is the extension functor
+#! The argument is a functor $F:C\to D$, and the output is the extension functor
 #! $F^\oplus:C^\oplus \to D^\oplus$.
 #! @Arguments F
-#! @Returns a CapFunctor
+#! @Returns a functor $C^\oplus \to D^\oplus$
 DeclareAttribute( "ExtendFunctorToAdditiveClosures",
                   IsCapFunctor );
 
 #! @Description
-#! The input is a functor $F:C\to D$, where $D$ is an additive category. The output is the extension functor
+#! The argument is a functor $F:C\to D$, where $D$ is an additive category. The output is the extension functor
 #! $F^\oplus:C^\oplus \to D$.
 #! @Arguments F
-#! @Returns a CapFunctor
+#! @Returns a functor $C^\oplus \to D$
 DeclareAttribute( "ExtendFunctorWithAdditiveRangeToFunctorFromAdditiveClosureOfSource",
                   IsCapFunctor );
 
 #! @Description
-#! The input is a functor $F:C\to D$. If $D$ is not known to be an additive category, then return
+#! The argument is a functor $F:C\to D$. If $D$ is not known to be an additive category, then return
 #! <C>ExtendFunctorToAdditiveClosures</C>(<A>F</A>), otherwise return
 #! <C>ExtendFunctorWithAdditiveRangeToFunctorFromAdditiveClosureOfSource</C>(<A>F</A>).
 #! @Arguments F
-#! @Returns a CapFunctor
+#! @Returns a functor $C^\oplus \to D^\oplus$ or $C^\oplus \to D$
 DeclareAttribute( "ExtendFunctorToAdditiveClosureOfSource",
                   IsCapFunctor );
 
@@ -118,37 +121,39 @@ DeclareAttribute( "ExtendFunctorToAdditiveClosureOfSource",
 ####################################
 
 #! @Description
-#! The input is some additive closure category $C^\oplus$. The output is $C$.
-#! @Arguments C^\oplus
-#! @Returns a CapCategory
+#! The argument is some additive closure category $A:=C^\oplus$. The output is $C$.
+#! @Arguments A
+#! @Returns the category $C$
 DeclareAttribute( "UnderlyingCategory",
                   IsAdditiveClosureCategory );
 
 #! @Description
-#! The input is a formal direct sum $A_1\oplus\dots\oplus A_m$. The output is the list $[A_1,\dots,A_m]$.
+#! The argument is a formal direct sum $A:=A_1\oplus\dots\oplus A_m$ in some additive closure category $C^\oplus$.
+#! The output is the list $[A_1,\dots,A_m]$.
 #! @Arguments A
-#! @Returns IsList
+#! @Returns a list of the objects in $C$
 DeclareAttribute( "ObjectList",
                   IsAdditiveClosureObject );
 
 #! @Description
-#! The input is a morphism $\alpha:A\to B$ between formal direct sums. The output is the defining matrix of $\alpha$.
+#! The argument is a morphism $\alpha:A\to B$ between formal direct sums in some additive closure category $C^\oplus$.
+#! The output is the defining matrix of $\alpha$.
 #! @Arguments alpha
-#! @Returns IsList
+#! @Returns a list of lists the morphisms in $C$
 DeclareAttribute( "MorphismMatrix",
                   IsAdditiveClosureMorphism );
 
 #! @Description
-#! The input is a morphism $\alpha:A\to B$ between formal direct sums. The output is the number of summands of the the source.
+#! The argument is a morphism $\alpha:A\to B$ between formal direct sums. The output is the number of summands of the the source.
 #! @Arguments alpha
-#! @Returns IsInt
+#! @Returns a non-negative integer
 DeclareAttribute( "NrRows",
                   IsAdditiveClosureMorphism );
 
 #! @Description
-#! The input is a morphism $\alpha:A\to B$ between formal direct sums. The output is the number of summands of the the range.
+#! The argument is a morphism $\alpha:A\to B$ between formal direct sums. The output is the number of summands of the the range.
 #! @Arguments alpha
-#! @Returns IsInt
+#! @Returns a non-negative integer
 DeclareAttribute( "NrColumns",
                   IsAdditiveClosureMorphism );
 
@@ -158,6 +163,11 @@ DeclareAttribute( "NrColumns",
 ##
 ####################################
 
+#! @Description
+#! The arguments are morphism $\alpha:A\to B$ between formal direct sums in some additive category $C^\oplus$ and an integer $i$.
+#! The output is the $i$'th entry in <C>MorphismMatrix</C>($\alpha$).
+#! @Arguments alpha, i
+#! @Returns a list of morphisms in $C$
 DeclareOperation( "\[\]",
                   [ IsAdditiveClosureMorphism, IsInt ] );
 
