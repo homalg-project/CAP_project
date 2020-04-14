58964996205f47d9c80a268fde246c3e3682bb12
Mohamed Barakat <mohamed.barakat@uni-siegen.de>
2020-04-14 16:04:02 +0200

    Update MonoidalCategories/gap/ClosedMonoidalCategories.gd
    Co-Authored-By: Sebastian Posur <sebastian.posur@rwth-aachen.de>

--- MonoidalCategories/gap/ClosedMonoidalCategories.gd
+++ MonoidalCategories/gap/ClosedMonoidalCategories.gd
@@ -797,6 +797,7 @@ DeclareOperation( "AddIsomorphismFromInternalHomToObjectWithGivenInternalHom",
 
 ##
 #! @Description
+#! This is a convenience method.
 #! The arguments are two cells $a,b$.
 #! The output is the internal hom cell .
 #! If $a,b$ are two CAP objects the output is the internal Hom object
