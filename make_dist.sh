685f8ba6d3509dfa3e3ae28ab3377c45ba2205f3
Fabian Zickgraf <f.zickgraf@dashdos.com>
2020-10-15 14:44:43 +0200

    Enable automated releases

---
+++ make_dist.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+packages="CAP ActionsForCAP AttributeCategoryForCAP CompilerForCAP ComplexesAndFilteredObjectsForCAP DeductiveSystemForCAP FreydCategoriesForCAP GeneralizedMorphismsForCAP GradedModulePresentationsForCAP GroupRepresentationsForCAP HomologicalAlgebraForCAP InternalExteriorAlgebraForCAP LinearAlgebraForCAP ModulePresentationsForCAP ModulesOverLocalRingsForCAP MonoidalCategories ToricSheaves "
+
+base_dir="$PWD"
+
+for pkg in ${packages}; do
+  ./release-gap-package --skip-existing-release --srcdir ${base_dir}/${pkg} --webdir ${base_dir}/gh-pages/${pkg} --update-file ${base_dir}/gh-pages/update.g $@
+done
