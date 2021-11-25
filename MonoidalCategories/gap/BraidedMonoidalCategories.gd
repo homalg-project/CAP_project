15a8c4bc310d4fe313502834c9cec37d320b66ad
Tom Kuhmichel <tom.kuhmichel@student.uni-siegen.de>
2021-11-25 16:38:15 +0100

    Braiding -> inverse braiding

--- MonoidalCategories/gap/BraidedMonoidalCategories.gd
+++ MonoidalCategories/gap/BraidedMonoidalCategories.gd
@@ -34,7 +34,7 @@ DeclareOperation( "BraidingWithGivenTensorProducts",
 
 #! @Description
 #! The arguments are two objects $a,b$.
-#! The output is the inverse of the braiding $ B_{a,b}^{-1}: b \otimes a \rightarrow a \otimes b$.
+#! The output is the inverse braiding $ B_{a,b}^{-1}: b \otimes a \rightarrow a \otimes b$.
 #! @Returns a morphism in $\mathrm{Hom}( b \otimes a, a \otimes b )$.
 #! @Arguments a,b
 DeclareOperation( "BraidingInverse",
@@ -44,7 +44,7 @@ DeclareOperation( "BraidingInverse",
 #! The arguments are an object $s = b \otimes a$, 
 #! two objects $a,b$,
 #! and an object $r = a \otimes b$.
-#! The output is the braiding $ B_{a,b}^{-1}: b \otimes a \rightarrow a \otimes b$.
+#! The output is the inverse braiding $ B_{a,b}^{-1}: b \otimes a \rightarrow a \otimes b$.
 #! @Returns a morphism in $\mathrm{Hom}( b \otimes a, a \otimes b )$.
 #! @Arguments s,a,b,r
 DeclareOperation( "BraidingInverseWithGivenTensorProducts",
