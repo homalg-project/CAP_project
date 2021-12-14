0b33db3f91a8acbce38eb42adbc8e8e04d87a65d
Mohamed Barakat <mohamed.barakat@rwth-aachen.de>
2021-12-14 21:03:31 +0100

    regenerated Toposes
    FROM MonoidalCategories v2021.12-07
    USING CategoryConstructor v2021.12-05

--- gap/CocartesianCoclosedCategories.autogen.gd
+++ gap/CocartesianCoclosedCategories.autogen.gd
@@ -450,7 +450,7 @@ DeclareOperation( "AddCoexponentialToCoproductAdjunctionMap",
 #! The arguments are a category $C$ and a function $F$.
 #! This operation adds the given function $F$
 #! to the category for the basic operation `CoproductToCoexponentialAdjunctionMap`.
-#! $F: ( b, c, g ) \mapsto \mathtt{CoproductToCoexponentialAdjunctionMap}(b, c, g)$.
+#! $F: ( c, b, g ) \mapsto \mathtt{CoproductToCoexponentialAdjunctionMap}(c, b, g)$.
 #! @Returns nothing
 #! @Arguments C, F
 DeclareOperation( "AddCoproductToCoexponentialAdjunctionMap",
