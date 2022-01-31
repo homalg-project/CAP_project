a43513f2d7f5ade26ec1ffe1bed09f7e1b7d5f47
Fabian Zickgraf <f.zickgraf@dashdos.com>
2022-01-31 14:22:54 +0100

    Update the documentation

--- CompilerForCAP/gap/CompilerForCAP.gd
+++ CompilerForCAP/gap/CompilerForCAP.gd
@@ -26,19 +26,18 @@ DeclareInfoClass( "InfoCapJit" );
 #! @Section Compiling a function manually
 
 #! @Description
-#!   Returns a compiled version of the function <A>func</A> (if <A>func</A> is an operation or a kernel function, it is returned unchanged).
-#!   The list of arguments <A>jit_args</A> is used to infer the types of variables.
-#!   If <A>jit_args</A> is shorter than the number of arguments accepted by <A>func</A>,
-#!   all steps which require knowledge about the types of variables are skipped.
-#!   If the first entry of <A>jit_args</A> is a CAP category, this category is used to get the type
-#!   information required to resolve CAP operations.
+#!   Returns a compiled version of the function <A>func</A> with signature <A>type_signature</A> (see <Ref Func="CapJitInferredDataTypes" />).
+#!   If <A>func</A> is an operation or a kernel function, it is returned unchanged.
+#!   If <A>type_signature</A> is not given, all steps which require knowledge about the types of variables are skipped.
+#!   If a full type signature is not available but the first argument of <A>func</A> is a CAP category, an instance of a CAP category can be given as the second argument.
+#!   In this case, the category is used to get the type information required to resolve CAP operations.
 #! @Returns a function
-#! @Arguments func, jit_args
+#! @Arguments func[, type_signature]
 DeclareGlobalFunction( "CapJitCompiledFunction" );
 
 #! @Description
 #!   Like <Ref Func="CapJitCompiledFunction" />, but returns an enhanced syntax tree of the compiled function.
 #!   <A>func</A> must not be an operation or a kernel function because those cannot properly be represented as a syntax tree.
 #! @Returns a record
-#! @Arguments func, jit_args
+#! @Arguments func[, type_signature]
 DeclareGlobalFunction( "CapJitCompiledFunctionAsEnhancedSyntaxTree" );
