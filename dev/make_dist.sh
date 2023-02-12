3af0922fcfa7198b5d1ebb7c072e7f7f3656c396
Mohamed Barakat <mohamed.barakat@rwth-aachen.de>
2023-02-12 15:56:51 +0100

    applied PackageJanitor

--- dev/make_dist.sh
+++ dev/make_dist.sh
@@ -7,5 +7,5 @@ packages="CAP ActionsForCAP AttributeCategoryForCAP CartesianCategories Compiler
 base_dir="$PWD"
 
 for pkg in ${packages}; do
-  ./release-gap-package --skip-existing-release --srcdir ${base_dir}/${pkg} --webdir ${base_dir}/gh-pages/${pkg} --update-script ${base_dir}/gh-pages/update.g $@
+  ./dev/release-gap-package --skip-existing-release --srcdir ${base_dir}/${pkg} --webdir ${base_dir}/gh-pages/${pkg} --update-script ${base_dir}/gh-pages/update.g --release-script ${base_dir}/dev/.release $@
 done
