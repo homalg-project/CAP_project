2e2442379f8f473e5733f8fd6c294ba999960d11
Fabian Zickgraf <f.zickgraf@dashdos.com>
2024-10-16 10:21:46 +0200

    Refactor the arguments of CapJitCompiledFunction(AsEnhancedSyntaxTree)

--- CompilerForCAP/gap/CompileCAPOperation.gd
+++ CompilerForCAP/gap/CompileCAPOperation.gd
@@ -9,7 +9,6 @@
 
 #! @Description
 #!   A special version of <Ref Func="CapJitCompiledFunctionAsEnhancedSyntaxTree" /> compiling the operation
-#!   given by <A>operation_name</A> in <A>cat</A>. The boolean <A>post_processing_enabled</A> decides
-#!   whether the tree before or after post-processing should be returned.
-#! @Arguments cat, operation_name, post_processing_enabled
+#!   given by <A>operation_name</A> in <A>cat</A>.
+#! @Arguments cat, operation_name
 DeclareGlobalFunction( "CapJitCompiledCAPOperationAsEnhancedSyntaxTree" );
